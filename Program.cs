f585c5a7fd3691cda2a83abe497de9c5a05d45a0
ReZvanYY <141616463+ReZvanYY@users.noreply.github.com>
2023-11-17 14:29:46 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TrueOrFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the first number, please!");
+            var numberOne = Console.ReadLine();
+
+            Console.WriteLine("Give me the second number, please!");
+            var numberTwo = Console.ReadLine();
+
+            bool isEqual = true;
+
+            if (numberOne == numberTwo)
+            {
+                isEqual = true;
+                Console.WriteLine("Equal numbers.");
+            }
+            else if (numberOne != numberTwo)
+            {
+                isEqual = false;
+                Console.WriteLine("Unequal numbers.");
+            }
+
+
+        }
+    }
+}
